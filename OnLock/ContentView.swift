090f2aef3355626c17df216fbf3f45fb109e85b1
James Valaitis <jamesvalaitis@gmail.com>
2021-03-11 10:18:29 +0000

    Initial Commit

---
+++ OnLock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnLock
+//
+//  Created by James Valaitis on 11/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OnLock/OnLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnLockApp.swift
+//  OnLock
+//
+//  Created by James Valaitis on 11/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct OnLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
